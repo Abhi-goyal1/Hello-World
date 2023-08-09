d1d22fce6aeda6fea5280c633ad3dd210e8c21e3
Abhi-goyal1 <abhigoyal72621289@gmail.com>
2023-08-09 17:52:22 +0530

    one more

--- script.js
+++ script.js
@@ -5,4 +5,19 @@ for(let link of links){
     if(link.dataset.active == bodyId){
         link.classList.add("active");
     }
-}
+};
+
+
+
+  // Get the current URL path
+  const currentPath = window.location.pathname;
+
+  // Get all tab links
+  const tabLinks = document.querySelectorAll('.nav-links');
+
+  // Iterate through tab links and add 'active' class to the matching tab
+  tabLinks.forEach(tabLink => {
+    if (tabLink.getAttribute('href') === currentPath) {
+      tabLink.classList.add('active');
+    }
+  });
--- tutorials/tutorials.js
+++ tutorials/tutorials.js
@@ -1,8 +1,12 @@
-let links = document.querySelectorAll(".tech-sectionl a");
-let bodyId = document.querySelector("html").id;
- 
-for(let link of links){
-    if(link.dataset.active == bodyId){
-        link.classList.add("active");
+  // Get the current URL path
+  const currentPath = window.location.pathname;
+
+  // Get all tab links
+  const tabLinks = document.querySelectorAll('.nav-links');
+
+  // Iterate through tab links and add 'active' class to the matching tab
+  tabLinks.forEach(tabLink => {
+    if (tabLink.getAttribute('href') === currentPath) {
+      tabLink.classList.add('active');
     }
-}
+  });
