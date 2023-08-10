d82518d6f7ce89f6df25cd59265f38083ee2be6d
Abhi-goyal1 <abhigoyal72621289@gmail.com>
2023-08-10 17:48:57 +0530

    sidebar

--- tutorials/tutorials.js
+++ tutorials/tutorials.js
@@ -10,3 +10,42 @@
       tabLink.classList.add('active');
     }
   });
+
+
+
+
+  let isSidebarOpen = false;
+  const sidebar = document.getElementById('sidebar');
+  const mainContainer = document.getElementById('main-container');
+
+  function toggleSidebar() {
+    if (isSidebarOpen) {
+      sidebar.style.left = '-250px';
+    } else {
+      sidebar.style.left = '0';
+    }
+    isSidebarOpen = !isSidebarOpen;
+  }
+
+  function showContent(page) {
+    let content = '';
+    if (page === 'home') {
+      content = '<h1>Welcome to the Home Page</h1><p>This is the main content area that will change when you click on the Home link.</p>';
+    } else if (page === 'about') {
+      content = '<h1>About Us</h1><p>This is the main content area that will change when you click on the About link.</p>';
+    } else if (page === 'services') {
+      content = '<h1>Our Services</h1><p>This is the main content area that will change when you click on the Services link.</p>';
+    } else if (page === 'contact') {
+      content = '<h1>Contact Us</h1><p>This is the main content area that will change when you click on the Contact link.</p>';
+    }
+    mainContainer.innerHTML = content;
+    resetActiveLinks();
+    document.getElementById(`${page}-link`).classList.add('active-link');
+  }
+
+  function resetActiveLinks() {
+    const links = document.querySelectorAll('.sidebar a');
+    links.forEach(link => {
+      link.classList.remove('active-link');
+    });
+  }
